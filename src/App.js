b4a49d1c177f6d1fbfaccb250068e30354c09151
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-10-22 18:09:51 -0300

    criado visualizador com botão para entrar no modo full-screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'aframe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Seja bem-vindo</h1>
+      <div className="image-contaienr">
+        <img src="https://i.postimg.cc/zvBNZc66/CwIEU.jpg" alt="imagem"/>
+        <a-scene embedded>
+          <a-sky src="https://i.postimg.cc/zvBNZc66/CwIEU.jpg" rotation="0 -90 0"></a-sky>
+        </a-scene>
+      </div>      
     </div>
   );
 }
