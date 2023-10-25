968b7502ce3c44562657999f02eb4cad9f28583e
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-10-25 01:19:00 -0300

    alterando imagem

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <h1>Seja bem-vindo</h1>
       <div className="image-contaienr">
-        <img src="https://i.postimg.cc/zvBNZc66/CwIEU.jpg" alt="imagem"/>
+        <img src="https://i.postimg.cc/cLG1H8dN/Panorama-piscina.jpg" alt="imagem"/>
         <a-scene embedded>
-          <a-sky src="https://i.postimg.cc/zvBNZc66/CwIEU.jpg" rotation="0 -90 0"></a-sky>
+          <a-sky src="https://i.postimg.cc/cLG1H8dN/Panorama-piscina.jpg" rotation="0 -90 0"></a-sky>
         </a-scene>
       </div>      
     </div>
