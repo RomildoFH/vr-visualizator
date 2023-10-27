ced21c33c06e1941f573783e497ac84f5758f156
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-10-27 19:37:17 -0300

    feat: Estilizando ícones para visualização

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { PiVirtualRealityFill } from 'react-icons/pi'
+import { TbAugmentedReality } from 'react-icons/tb'
 import './App.css';
 import 'aframe';
 
 function App() {
   const sceneRef = useRef(null);
+  const [url, setUrl] = useState('https://i.postimg.cc/d3LCTf90/vr-example-fixed.jpg');
+  const [vrUrl, setVrUrl] = useState('https://i.postimg.cc/cLG1H8dN/Panorama-piscina.jpg');
 
   const handleEnterVRClick = () => {
     if (sceneRef.current) {
@@ -18,22 +22,31 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <main className="App">
       <h1>Seja bem-vindo</h1>
-      <div className="image-container">
-        <img
-          src="https://i.postimg.cc/d3LCTf90/vr-example-fixed.jpg"
-          alt="Imagem 360"
-          onClick={handleEnterVRClick}
-          className="image-preview"
-        />
-        <button onClick={handleEnterVRClick}>Entrar no VR</button>
-        <button onClick={handleEnterARClick}>Entrar no AR</button>
+      <section id="image-preview-container">
+        <div className="image-container">
+          <img
+            src={url}
+            alt="Imagem 360"
+            onClick={handleEnterVRClick}
+            className="image-preview"
+          />
+          <div className="image-controllers">
+            <button onClick={handleEnterVRClick} className="controller">
+              <PiVirtualRealityFill size={50} />
+            </button>
+            <button onClick={handleEnterARClick} className="mobile-only controller">
+              <TbAugmentedReality size={50} />
+            </button>
+          </div>
+        </div>
+        <p>Para visualizar a imagem, clique nela ou em algum dos botões ao lado</p>
+      </section>
         <a-scene ref={sceneRef} embedded vr-mode-ui="enabled: false">
-          <a-sky src="https://i.postimg.cc/cLG1H8dN/Panorama-piscina.jpg" rotation="0 -90 0"></a-sky>
+          <a-sky src={vrUrl} rotation="0 -90 0"></a-sky>
         </a-scene>
-      </div>
-    </div>
+    </main>
   );
 }
 
