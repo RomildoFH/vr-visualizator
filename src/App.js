798b7177d77eca4d2f5803917ab061a4af9e2a76
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-10-27 15:09:20 -0300

    feat: criados botões personalizados para acesso ao VR e AR

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 import 'aframe';
 
 function App() {
+  const sceneRef = useRef(null);
+
+  const handleEnterVRClick = () => {
+    if (sceneRef.current) {
+      sceneRef.current.enterVR();
+    }
+  };
+
+  const handleEnterARClick = () => {
+    if (sceneRef.current) {
+      sceneRef.current.enterAR();
+    }
+  };
+
   return (
     <div className="App">
       <h1>Seja bem-vindo</h1>
-      <div className="image-contaienr">
-        <img src="https://i.postimg.cc/zvBNZc66/CwIEU.jpg" alt="imagem"/>
-        <a-scene embedded>
+      <div className="image-container">
+        <img
+          src="https://i.postimg.cc/zvBNZc66/CwIEU.jpg"
+          alt="Imagem 360"
+          onClick={handleEnterVRClick}
+          className="image-preview"
+        />
+        <button onClick={handleEnterVRClick}>Entrar no VR</button>
+        <button onClick={handleEnterARClick}>Entrar no AR</button>
+        <a-scene ref={sceneRef} embedded vr-mode-ui="enabled: false">
           <a-sky src="https://i.postimg.cc/zvBNZc66/CwIEU.jpg" rotation="0 -90 0"></a-sky>
         </a-scene>
-      </div>      
+      </div>
     </div>
   );
 }
